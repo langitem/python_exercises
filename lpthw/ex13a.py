53197fc6334ee9511e5fb71d2a83cf411bd1cc1a
Emanuel Langit <emanuel.langit@fastmail.fm>
2015-11-28 11:39:44 -0800

    Finished up to ex15

---
+++ lpthw/ex13a.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from sys import argv
+
+script, first, second = argv
+
+print "The script is called:", script
+print "The first argument is:", first
+print "The second argument is:", second
